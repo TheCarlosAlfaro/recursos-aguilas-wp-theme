775320cba03c8f0d805514b156f28d05795467f1
devbysalas <rgkalin@gmail.com>
2018-10-04 10:49:50 -0700

    finished jumbotron area

---
+++ global-templates/jumbotron.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Jumbotron setup.
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <div class="row">
+      <div class="col-md text-md-left text-center">
+        <h1 class="display-4">BUILD A HEALTHY CHURCH</h1>
+        <p class="lead">“Spiritual health is the key to church growth, but healthy churches don’t happen by accident. They are nurtured over time by intentional and passionate pastors and leaders who practice Christian leadership, spiritual formation, and biblical decision making.”</p>
+        
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModal">
+        <i class="fa fa-play" aria-hidden="true"></i> WATCH OUR VIDEO
+        </button>
+
+              <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <script src="https://fast.wistia.com/embed/medias/902avzvnyy.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_902avzvnyy seo=false videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/902avzvnyy/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="col-md floating-img-container">
+        <img src="http://recursos-aguilascfc.local/wp-content/uploads/2018/10/Morris_Robert_3_Clear_BG_2.png" alt="" class="floating-img">
+      </div>
+    </div>
+  </div>
+</div>
