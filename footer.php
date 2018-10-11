afe5f28c4321a00e69558e8ac5b300c326cc0c49
devbysalas <rgkalin@gmail.com>
2018-10-10 17:26:39 -0700

    Finesh DOM manipulation for search

--- footer.php
+++ footer.php
@@ -48,10 +48,15 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<div class="search-overlay__top">
 		<div class="container">
 			<i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-			<input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+			<input type="text" class="search-term" placeholder="¿Qué está buscando?" id="search-term">
 			<i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
 		</div>
 	</div>
+
+	<div class="container">
+		<div id="search-overlay__results"></div>
+	</div>
+
 </div>
 
 <?php wp_footer(); ?>
